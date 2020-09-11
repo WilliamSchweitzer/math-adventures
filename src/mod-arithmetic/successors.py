ce6aa22cd79ab5aa9fd00bc623c08455b7531586
Will Schweitzer <weschweitzer@outlook.com>
2020-09-11 04:28:58 -0400

    update readme

--- src/mod-arithmetic/successors.py
+++ src/mod-arithmetic/successors.py
@@ -42,11 +42,33 @@ def modular_add(x, y, mod):
  else:
   print(0)
 
-print('Modular addition/subtraction test case')
+print('Modular addition/subtraction test case, subtraction works the same way -> works for any amount of additions/subtractions & any amount of mods')
+print('Example (((a+b+c) mod m) + d) mod m)') 
 print('x=14, y=16, mod=19, x + y == result == 30/19 == 11, 30 mod 19 == 11')
 print('___________________________________________________________________')
 
 modular_add(14,16,19)
 
+print('Test case: result < mod, 1+7 mod 19 = 11')
 
+modular_add(1,7,19)
 
+# Given a number m outside of our range we divide by mod p -> m mod p
+# IMPORTANT -> Congruence operator denotes equivalence in mod arithmetic, if difference is multiple of 3
+
+def check_congruence(x1, m1, x2, m2):
+ try:
+  if m1 != m2:
+   print('Mod values are not equal!')
+
+  first_value = modular_add(int(x1),0,int(m1))
+  second_value = modular_add(int(x2),0,int(m2))
+  print('Always true if mod is the same')
+ except:
+  print('Mod value may not have been the same?')
+
+print('Test case: x1=7, x2=10 m1,m2=3')
+check_congruence(7, 3, 10, 3)
+
+print('Test case: m1,m2 are different')
+check_congruence(7, 3, 10, 4)
