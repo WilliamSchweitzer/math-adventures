62f297b0fa0ab8191f289ac157ec52f92b6fae90
Will Schweitzer <weschweitzer@outlook.com>
2020-09-15 12:49:52 -0400

    start cyclic groups

---
+++ src/groups.py
@@ -0,0 +1,19 @@
+# Learning about groups in modular arithmetic
+
+"""
+Prime number:
+ - A positive integer greater than 1
+ - it's gcd is 1, and itself
+
+ 30 of the smallest prime numbers:
+ 2, 3, 5, 11, 13, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113
+ 
+ The spacing increases greatly as prime numbers increase.
+
+ Group:
+ Example: integers from 0-9 and addition modulo 10
+ - closed
+ - have an identity
+ - are invertible
+ - are assosiative
+"""
